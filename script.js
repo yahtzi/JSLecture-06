4b5c35c384134e985ac3dba30e9c2071bbf2ec75
yahtzi <yaritzabejarano7220@my.whccd.com>
2020-01-27 21:17:52 -0800

    Declared value to myGrade

---
+++ script.js
@@ -0,0 +1,4 @@
+//Declare a new variable "myGrade" and assign a number value from 0-100 to it//
+let myGrade = 85;
+
+//Write an if/else-if/else statement that displays the letter grade using console.log(...) (A for 90+, B for 80-89, C for 70-79, D for 60-69, F for 59 and below)//
